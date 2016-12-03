e1d76931421be62b6d57d1fd15e43703c09a2f23
FlorianUbink <FlorianUbink@users.noreply.github.com>
2016-12-03 02:49:55 +0100

    I

---
+++ ComponentSystem/ComponentSystem/Bag.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentSystem
+{
+    public class Bag
+    {
+
+
+        public int objectid { get; set; }
+
+        public Vector2 position { get; set; }
+
+    }
+}
---
+++ ComponentSystem/ComponentSystem/GameObject.cs
@@ -0,0 +1,27 @@
+﻿using ComponentSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentSystem
+{
+    public class GameObject
+    {
+        #region Fields
+        Bag myBag;
+        #endregion
+
+        public GameObject(int objectid)
+        {
+            myBag = new Bag();
+            myBag.objectid = objectid;
+        }
+
+        public void Distroy()
+        {
+            myBag = null;
+        }
+    }
+}
